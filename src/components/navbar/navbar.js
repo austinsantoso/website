64a9b8c75d888099b1738911a29a3dbf5b3eacbd
austinsantoso <santoso.austin@gmail.com>
2022-02-27 18:23:18 +0800

    added files

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,16 @@
+function Navbar() {
+    return (
+        <nav>
+            <h1>Austin Santoso</h1>
+
+            <div>
+                <a href="/#about"> About me</a>
+                <a href="/#experiences"> Experiences</a>
+                <a href="/#projects"> Projects</a>
+                <a href="/contact"> Contact</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
